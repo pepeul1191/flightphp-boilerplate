e4fe0e11a5e041c6e0b3c8ae255fa752ed957325
Pepe Valdivia <jvaldivia@softweb.pe>
2018-05-11 11:47:03 -0500

    Comenzando a integrar vistas con Smarty

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ header('Server: Ubuntu');
 
 Flight::set('config', App\Config\Constants);
 Flight::set('flight.views.path', 'app/views');
+Flight::register('view', 'Smarty', array(), function($smarty){
+  $smarty->template_dir = 'app/templates/';
+  $smarty->compile_dir = 'app/templates_c/';
+  $smarty->config_dir = 'app/config/';
+  $smarty->cache_dir = 'app/cache/';
+});
 Flight::route('GET /', array('App\Controllers\Home','index'));
 Flight::route('GET /demo', array('App\Controllers\Demo','hello'));
 Flight::route('GET /departamento/listar', array('App\Controllers\Departamento','listar'));
